2ae6ae089fc89c66184fe33f2d9140c0a0c0d012
Hiroaki ITO <mail@isudzumi.com>
2021-12-24 00:56:33 +0900

    Add grammar to pass subroutine test

--- grammar.js
+++ grammar.js
@@ -1,6 +1,29 @@
 module.exports = grammar({
     name: 'vcl',
     rules: {
-        source_file: $ => 'hello'
+        program: $ => repeat($.subroutine),
+
+        subroutine: $ => seq(
+          'sub',
+          field('name', $.builtin_action),
+          $.statement_block
+        ),
+
+        builtin_action: $ => choice(
+          'vcl_fetch',
+          'vcl_hash',
+          'vcl_hit',
+          'vcl_pass',
+          'vcl_purge',
+          'vcl_pipe',
+          'vcl_miss',
+          'vcl_deliver',
+          'vcl_error'
+        ),
+
+        statement_block: $ => prec.right(seq(
+          '{',
+          '}'
+        )),
     }
 })
